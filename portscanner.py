6243d2a83d40048af970719434151d962a06110c
Tom <tomas@thirdpartytrust.com>
2019-05-24 00:03:26 -0300

    Add waiter, time analisis and code cleanup

--- portscanner.py
+++ portscanner.py
@@ -1,11 +1,16 @@
+from time import sleep
 from pprint import pprint as pp
 from socket import (AF_INET, AF_INET6, SOCK_DGRAM, SOCK_STREAM, error,
                     gaierror, socket)
 from argparse import ArgumentParser
+from datetime import datetime
 from subprocess import call
 
+from _thread import start_new_thread
+
 MAX_PORT = 65535
-COMMON_PORTS = []
+COMMON_PORTS = 1023
+MAX_DOTS = 10
 LOCALHOST = '127.0.0.1'
 
 PORT_TYPE = {
@@ -17,6 +22,9 @@ HOST_TYPE = {
     'IPV4': AF_INET,
     'IPV6': AF_INET6,
 }
+
+scan_type = 'specific'
+start = datetime.now()
 
 parser = ArgumentParser()
 
@@ -49,32 +57,79 @@ parser.add_argument(
     )
 args = parser.parse_args()
 
-port_type = PORT_TYPE.get(args.pt)
-host_type = HOST_TYPE.get(args.ht)
+p_type = PORT_TYPE.get(args.pt)
+h_type = HOST_TYPE.get(args.ht)
 opened = []
 close = []
 
 call('clear', shell=True)
 
-if args.f:
-    for port in range(MAX_PORT):
-        pass
-elif args.c:
-    for port in COMMON_PORTS:
-        pass
-else:
+
+def waiter():
+    """Waiter prints while scanning."""
+    for i in range(MAX_PORT):
+        print('Scanning: ', end='', flush=True)
+        for _ in range(MAX_DOTS):
+            print('.', end='', flush=True)
+            sleep(0.1)
+        print('\n')
+        call('clear', shell=True)
+    print('\n')
+    call('clear', shell=True)
+
+
+def connect(host, port, host_type, port_type):
+    """Attempts to connect to a port on a given host.
+    :param host: host (str) e.g 127.0.0.1
+    :param port: port (int) e.g 80
+    :param host_type: connection host type (obj) e.g AF_INET
+    :param port_type: connection port type (obj) e.g SOCK_STREAM
+    """
+    port_type_name = [k for k, v in PORT_TYPE.items() if v == port_type][0]
     try:
         s = socket(host_type, port_type)
-        s.connect((args.h, int(args.p)))
-        opened.append((args.h, args.p))
+        s.connect((host, port))
+        s.close()
+        opened.append((host, port, port_type_name))
     except gaierror:
         print("Can't resolve hostname")
     except error:
-        close.append((args.h, args.p))
+        close.append((host, port, port_type_name))
     except ConnectionRefusedError:
-        close.append((args.h, args.p))
+        close.append((host, port, port_type_name))
 
-print('Open:\n')
+
+def special_scans(ports_amount):
+    """Given an amount of ports, analize if open or close.
+    :params ports_amount: (int) max amount of ports to analyze
+    """
+    for port in range(ports_amount):
+        if args.pt == 'ALL':
+            for ptype in PORT_TYPE.values():
+                connect(args.h, port, h_type, ptype)
+        else:
+            connect(args.h, port, h_type, p_type)
+
+
+if args.f:
+    scan_type = 'full'
+    start_new_thread(waiter, ())
+    special_scans(MAX_PORT)
+elif args.c:
+    scan_type = 'common'
+    start_new_thread(waiter, ())
+    special_scans(COMMON_PORTS)
+else:
+    connect(args.h, int(args.p), h_type, p_type)
+
+end = datetime.now()
+delta = end - start
+
+call('clear', shell=True)
+print('Scan type: {}'.format(scan_type))
+print('Scan took: {} seconds'.format(delta.seconds))
+print('Open:')
+print('== Amount of open ports: {}'.format(len(opened)))
 pp(opened)
-print('Closed:\n')
-pp(close)
+print('\nClosed:')
+print('== Amount of close ports: {}'.format(len(close)))
